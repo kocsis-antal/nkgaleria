9e975b51681b479b6f4246cc28c2a91a9101563a
kocsi <kocsis.antal@gmail.com>
2020-01-22 23:00:21 +0100

    Generátor pontosítások + fontok

--- src/main/java/xyz/kocsisantal/pdf/Generator.java
+++ src/main/java/xyz/kocsisantal/pdf/Generator.java
@@ -1,8 +1,7 @@
 package xyz.kocsisantal.pdf;
 
-import java.io.File;
-import java.io.IOException;
-
+import org.apache.fontbox.ttf.OTFParser;
+import org.apache.fontbox.ttf.OpenTypeFont;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
@@ -10,9 +9,11 @@ import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType0Font;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Generator {
-    public static final float SIDE_MARGIN = 20;
-    public static final float WIDTH = 420;
+    public static final float WIDTH = 420f;
     public static final String HELYE_STRING = "Helye: ";
     private static final float BOX_WIDTH = 1134;
     private final String name;
@@ -28,31 +29,33 @@ public class Generator {
     public void generate(final File file) throws IOException {
         // create document
         try (final PDDocument document = new PDDocument()) {
-            final PDType0Font fontNormal = PDType0Font.load(document, new File("c:/windows/fonts/arial.ttf"));
-            final PDType0Font fontBold = PDType0Font.load(document, new File("c:/windows/fonts/arialbd.ttf"));
+            final PDType0Font fontNormal = PDType0Font.load(document, parseFont("/ZAPFELLIPTICAL711PFL.OTF"), false);
+            final PDType0Font fontBold = PDType0Font.load(document, parseFont("/ZAPFELLIPTICAL711PFL-BOLD.OTF"), false);
+            final PDType0Font fontItalic = PDType0Font.load(document, parseFont("/ZAPFELLIPTICAL711PFL-ITALIC.OTF"), false);
+            final PDType0Font fontBoldItalic = PDType0Font.load(document, parseFont("/ZAPFELLIPTICAL711PFL-BOLDITALIC.OTF"), false);
 
             // create page
-            final PDPage currentPage = new PDPage(new PDRectangle(WIDTH, 1134));
+            final PDPage currentPage = new PDPage(new PDRectangle(WIDTH, 1133.86f));
             document.addPage(currentPage);
 
             // start the page stream
             try (final PDPageContentStream contentStream = new PDPageContentStream(document, currentPage)) {
 
                 contentStream.beginText();
-                final float linePointer = 115;
+                final float linePointer = 114;
 
                 // name
-                final float nameFontSize = calculateSize(HELYE_STRING + name, fontBold, 15);
+                final float nameFontSize = calculateSize(HELYE_STRING + name, fontBold, 14f);
 
-                contentStream.newLineAtOffset(WIDTH / 2 - calculateTextWidth(HELYE_STRING, fontNormal, nameFontSize) / 2 - calculateTextWidth(name, fontBold, nameFontSize) / 2, linePointer);
-                contentStream.setFont(fontNormal, nameFontSize);
+                contentStream.newLineAtOffset(WIDTH / 2 - calculateTextWidth(HELYE_STRING, fontItalic, nameFontSize) / 2 - calculateTextWidth(name, fontBold, nameFontSize) / 2, linePointer);
+                contentStream.setFont(fontItalic, nameFontSize);
                 contentStream.showText(HELYE_STRING);
                 contentStream.setFont(fontBold, nameFontSize);
                 contentStream.showText(name);
                 contentStream.endText();
 
                 // address
-                final float addressFontSize = calculateSize(address, fontNormal, 10);
+                final float addressFontSize = calculateSize(address, fontNormal, 12.5f);
 
                 contentStream.beginText();
                 contentStream.newLineAtOffset(WIDTH / 2 - calculateTextWidth(address, fontNormal, addressFontSize) / 2, linePointer - 17.5f);
@@ -61,7 +64,7 @@ public class Generator {
                 contentStream.endText();
 
                 // date
-                final float dateFontSize = calculateSize(date, fontNormal, 10);
+                final float dateFontSize = calculateSize(date, fontNormal, 12.5f);
 
                 contentStream.beginText();
                 contentStream.newLineAtOffset(WIDTH / 2 - calculateTextWidth(date, fontNormal, dateFontSize) / 2, linePointer - 17.5f * 2);
@@ -71,8 +74,8 @@ public class Generator {
 
                 // footer
                 contentStream.beginText();
-                contentStream.newLineAtOffset(SIDE_MARGIN, linePointer - 17.5f * 3);
-                contentStream.setFont(fontNormal, 10);
+                contentStream.newLineAtOffset(18, 60);
+                contentStream.setFont(fontNormal, 12);
                 contentStream.showText("Tel.: 36-1/267-52-62, 36-20/265-25-49, 36-20/9-156-076, 36-20/366-8000");
                 contentStream.endText();
             }
@@ -81,15 +84,19 @@ public class Generator {
         }
     }
 
+    private OpenTypeFont parseFont(String fileName) throws IOException {
+        return new OTFParser(true).parse(new File(this.getClass().getResource(fileName).getFile()));
+    }
+
     private float calculateSize(final String text, final PDFont font, float size) throws IOException {
         while (calculateTextWidth(text, font, size) > BOX_WIDTH) {
-            size -= 0.1;
+            size -= 0.1f;
         }
 
         return size;
     }
 
     public float calculateTextWidth(final String text, final PDFont font, final float size) throws IOException {
-        return font.getStringWidth(text) / 1000 * size;
+        return font.getStringWidth(text) / 1000.0f * size;
     }
 }
